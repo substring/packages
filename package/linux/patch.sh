6ce69f219749cd403c9cad5a6e263f99b63a5adc
Subs <frog2wah@gmail.com>
2019-08-10 10:55:26 +0000

    Fix trunk branch remains

--- package/linux/patch.sh
+++ package/linux/patch.sh
@@ -6,8 +6,8 @@ sed -i \
 
 # We need to patch the config. So, find its hash first, patch, recompute hash, then patch PKGBUILD
 # Keep the code commented, if we ever need to patch config later
-#oldhash=$(sha256sum /work/linux/trunk/config | cut -d ' ' -f1)
-#sed -i "/CONFIG_DRM_KMS_HELPER=/s/m$/y/" /work/linux/trunk/config || exit 1
-#newhash=$(sha256sum /work/linux/trunk/config | cut -d ' ' -f1)
-#sed -i "s/$oldhash/$newhash/" /work/linux/trunk/PKGBUILD || exit 1
+#oldhash=$(sha256sum /work/repos/core-x86_64/config | cut -d ' ' -f1)
+#sed -i "/CONFIG_DRM_KMS_HELPER=/s/m$/y/" /work/linux/repos/core-x86_64/config || exit 1
+#newhash=$(sha256sum /work/linux/repos/core-x86_64/config | cut -d ' ' -f1)
+#sed -i "s/$oldhash/$newhash/" /work/linux/repos/core-x86_64/PKGBUILD || exit 1
 
