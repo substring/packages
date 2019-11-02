0abbd5efc4e5baba2bea82a247905176dfbb960f
Subs <frog2wah@gmail.com>
2019-11-02 10:39:10 +0100

    Adapt to linux 5.3.8

--- package/linux/patch.sh
+++ package/linux/patch.sh
@@ -1,13 +1,13 @@
 patch -p2 -d /work/linux < /work/package/linux/patch/PKGBUILD.patch || exit 1
 sed -i \
 -e '/_srcname=archlinux-linux/a _kernelversion="$(echo ${_srcver%-*} | cut -d '.' -f 1,2)"' \
--re 's/pkgbase=linux[[:space:]]+/pkgbase=linux-15khz /' \
+-re 's/pkgbase=linux/pkgbase=linux-15khz/' \
 /work/linux/repos/core-x86_64/PKGBUILD || exit 1
 
 # We need to patch the config. So, find its hash first, patch, recompute hash, then patch PKGBUILD
 # Keep the code commented, if we ever need to patch config later
-#oldhash=$(sha256sum /work/repos/core-x86_64/config | cut -d ' ' -f1)
-#sed -i "/CONFIG_DRM_KMS_HELPER=/s/m$/y/" /work/linux/repos/core-x86_64/config || exit 1
+#oldhash=$(sha256sum /work/linux/repos/core-x86_64/config | cut -d ' ' -f1)
+#sed -i "/CONFIG_LOCALVERSION_AUTO=/s/m$/n/" /work/linux/repos/core-x86_64/config || exit 1
 #newhash=$(sha256sum /work/linux/repos/core-x86_64/config | cut -d ' ' -f1)
 #sed -i "s/$oldhash/$newhash/" /work/linux/repos/core-x86_64/PKGBUILD || exit 1
 
