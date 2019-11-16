ec542c3da126220177f4e240c3c888d98c7d2281
Subs <frog2wah@gmail.com>
2019-11-16 19:30:11 +0100

    linux: package should be more robust to changes upstream

--- package/linux/patch.sh
+++ package/linux/patch.sh
@@ -3,6 +3,7 @@ sed -i \
 -e '/_srcname=archlinux-linux/a _kernelversion="$(echo ${pkgver} | cut -d '.' -f 1,2)"' \
 -re 's/pkgbase=linux/pkgbase=linux-15khz/' \
 /work/linux/repos/core-x86_64/PKGBUILD || exit 1
+( cd /work/linux/repos/core-x86_64 && makepkg -g >> PKGBUILD )
 
 # We need to patch the config. So, find its hash first, patch, recompute hash, then patch PKGBUILD
 # Keep the code commented, if we ever need to patch config later
