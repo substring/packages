1c4a7d49ddd192e4ac1a5a17f37088c9e8aedb5c
Subs <frog2wah@gmail.com>
2020-02-02 15:50:23 +0100

    Bump mame to 0.218

--- package/mame/patch.sh
+++ package/mame/patch.sh
@@ -1,3 +1,2 @@
 # Get mame version
-version=`grep 'pkgver=' /work/build/mame/repos/community-x86_64/PKGBUILD | cut -d "=" -f 2 | tr -d '.'`
 patch -p1 -d /work/build < /work/package/mame/patch/PKGBUILD.patch
