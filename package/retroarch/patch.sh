809bd874795d4624f3c11be30bd8a96f2d8cda09
Subs <frog2wah@gmail.com>
2022-01-13 20:00:27 +0100

    Retroarch: new package superseeding Arch' for KMS modeswitching support

---
+++ package/retroarch/patch.sh
@@ -0,0 +1,10 @@
+# Find the current pkgrel
+pkgrel="$(egrep "^pkgrel=" /work/build/retroarch/repos/community-x86_64/PKGBUILD | cut -d '=' -f 2)"
+# Increase it by one so we don't overlap with the real Arch pkg
+pkgrel=$((pkgrel+1))
+patch -Np1 -d /work/build/retroarch < /work/package/retroarch/patch/PKGBUILD.patch
+[[ $? -gt 0 ]] && exit 1
+sed -i \
+  -e "s/pkgrel=.*/pkgrel=${pkgrel}/" \
+  /work/build/retroarch/repos/community-x86_64/PKGBUILD || exit 1
+( cd /work/build/retroarch/repos/community-x86_64 && makepkg -g >> PKGBUILD )
