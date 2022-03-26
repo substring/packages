99cd44e072617babdc77e82bfabaff575825d09d
Subs <frog2wah@gmail.com>
2022-03-26 14:06:23 +0100

    sdl2: workaround the udev bug SDL2 would fail if KMS is started after a X session. This is not a nice patch, but will be ok for now

--- package/sdl2/patch.sh
+++ package/sdl2/patch.sh
@@ -1,7 +1,7 @@
 # Find the current pkgrel
 pkgrel="$(egrep "^pkgrel=" /work/build/sdl2/repos/extra-x86_64/PKGBUILD | cut -d '=' -f 2)"
 # Increase it by one so we don't overlap with the real Arch pkg
-pkgrel=$((pkgrel+2))
+pkgrel=$((pkgrel+3))
 patch -Np1 -d /work/build/sdl2 < /work/package/sdl2/patch/PKGBUILD.patch
 [[ $? -gt 0 ]] && exit 1
 sed -i \
