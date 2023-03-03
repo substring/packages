c54cf50efb52478df3897a1427f4e4aaa0adc3e9
Subs <frog2wah@gmail.com>
2023-03-03 22:34:15 +0100

    linux: fix 6.2.1 not booting EFI syslinux See FS#77632

--- package/linux/patch.sh
+++ package/linux/patch.sh
@@ -1,5 +1,6 @@
 patch -Np2 -d /work/build/linux < /work/package/linux/patch/PKGBUILD.patch
 [[ $? -gt 0 ]] && exit 1
+sed -i "s/# CONFIG_EFI_HANDOVER_PROTOCOL is not set/CONFIG_EFI_HANDOVER_PROTOCOL=y/" /work/build/linux/repos/core-x86_64/config || exit 1
 sed -i \
 -e '/pkgver=.*/a _kernelversion="$(echo ${pkgver} | cut -d '.' -f 1,2)"' \
 -re 's/pkgbase=linux.*/pkgbase=linux-15khz/' \
@@ -22,4 +23,3 @@ sed -i \
 #sed -i "/CONFIG_LOCALVERSION_AUTO=/s/m$/n/" /work/build/linux/repos/core-x86_64/config || exit 1
 #newhash=$(sha256sum /work/build/linux/repos/core-x86_64/config | cut -d ' ' -f1)
 #sed -i "s/$oldhash/$newhash/" /work/build/linux/repos/core-x86_64/PKGBUILD || exit 1
-
