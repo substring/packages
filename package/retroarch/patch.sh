4d67edf6c7b32d452802c8f9c12b77cb176fe1cf
Subs <frog2wah@gmail.com>
2023-06-20 00:18:47 +0200

    retroarch: slight bump for tate AR fix

--- package/retroarch/patch.sh
+++ package/retroarch/patch.sh
@@ -1,7 +1,7 @@
 # Find the current pkgrel
 pkgrel="$(egrep "^pkgrel=" /work/build/retroarch/PKGBUILD | cut -d '=' -f 2)"
 # Increase it by one so we don't overlap with the real Arch pkg
-pkgrel=$((pkgrel+1))
+pkgrel=$((pkgrel+2))
 patch -tNp3 -d /work/build/retroarch < /work/package/retroarch/patch/PKGBUILD.patch
 [[ $? -gt 0 ]] && exit 1
 sed -i \
