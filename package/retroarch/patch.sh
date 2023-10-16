79ccdc1b1739b9b7dedd6d217e3181367dd694df
Subs <frog2wah@gmail.com>
2023-10-16 15:28:08 +0200

    retroarch: force a rebuild The Arch package crashes with a buffer overflow on KMS

--- package/retroarch/patch.sh
+++
@@ -1,4 +0,0 @@
-# When building a specific commit, git tags --describe doesn't return just the tag, and this breaks pkgver()
-sed -i \
-  -e "s+git describe --tags+git describe --tags --abbrev=0+" \
-  /work/build/retroarch/PKGBUILD || exit 1
