103f667d27303b904051f7e7c6c96911762f3fd0
Substring <frog2wah@gmail.com>
2024-02-16 22:52:53 +0100

    retroarch: fix buffer overflow

--- package/retroarch/retroarch.pp
+++ package/retroarch/retroarch.pp
@@ -1,1 +1,8 @@
-pkgrel++
+pkgrel+++
+source+="fix_drm_buffer_overflow.patch"
+prepare() {
+  cd RetroArch
+  patch -Np1 -i ../retroarch-config.patch
+  patch -Np1 -i ../fix_drm_buffer_overflow.patch
+}
+
