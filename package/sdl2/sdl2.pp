de37a1ede06b19a539ef1c152b0a73714a0e59ec
Substring <frog2wah@gmail.com>
2024-07-10 21:10:55 +0200

    sdl2: fix SDL2.30.5 until 2.30.6 is released

--- package/sdl2/sdl2.pp
+++ package/sdl2/sdl2.pp
@@ -1,11 +1,13 @@
-pkgrel++
+pkgrel+++
 source+="force_modes_refresh.patch"
 source+="sdl_restore_crtc_at_DestroyWindow.patch"
-
+source+="https://patch-diff.githubusercontent.com/raw/libsdl-org/SDL/pull/10219.patch"
 prepare() {
   cd SDL2-${pkgver}
   msg2 "Applying force_modes_refresh.patch"
   patch -Np1 -i "$srcdir"/force_modes_refresh.patch
   msg2 "Applying sdl_restore_crtc_at_DestroyWindow.patch"
   patch -Np1 -i "$srcdir"/sdl_restore_crtc_at_DestroyWindow.patch
+  msg2 "Applying 10219.patch"
+  patch -Np1 -i "$srcdir"/10219.patch
 }
