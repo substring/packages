b1dfaba61fef617a64175861dc1e0e5ca0e8a2e3
Substring <frog2wah@gmail.com>
2024-12-29 21:53:05 +0100

    CI: fix broadcom-wl dkms automatically zstd the resulting module if the current kernel modules are compressed. This makes install fail in PKGBUILD

--- package/broadcom-wl/patch.sh
+++ package/broadcom-wl/patch.sh
@@ -1,2 +1,4 @@
 set -x
 sed -i 's/^_kernelname=/&-15khz/' /work/build/broadcom-wl/PKGBUILD || exit 1
+# This is a possible fix for dkms aut-zstd-ing the built module, which fails the build
+#sed -i 's+module/\*\.ko+module/*.ko*+' /work/build/broadcom-wl/PKGBUILD || exit 1
