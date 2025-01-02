49413b214e96a41f31d601d9b4ea78607fca1daf
Substring <frog2wah@gmail.com>
2025-01-02 22:57:25 +0100

    linux: add DCN3 patch

--- package/linux/linux.pp
+++ package/linux/linux.pp
@@ -1,8 +1,9 @@
 pkgbase=linux-15khz
+options=(debug !strip)
 _kernelversion="$(echo ${pkgver} | cut -d '.' -f 1,2)"
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/01_linux_15khz.patch
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/02_linux_15khz_interlaced_mode_fix.patch
-source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/03_linux_15khz_dcn1_dcn2_interlaced_mode_fix.patch
+source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/03_linux_15khz_dcn1_dcn2_dcn3_interlaced_mode_fix.patch
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/04_linux_15khz_dce_interlaced_mode_fix.patch
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/05_linux_15khz_amdgpu_pll_fix.patch
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/06_linux_switchres_kms_drm_modesetting.patch
