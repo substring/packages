aca5b37177a23d6e8a4cab1b0ba94b5de0bf8c4c
Substring <frog2wah@gmail.com>
2025-02-12 13:43:31 +0100

    filesystem: wrong path for the neofetch ascii logo

--- package/filesystem/aliases.sh
+++ package/filesystem/aliases.sh
@@ -3,4 +3,4 @@
 alias ls='ls --color=auto'
 alias startx="startx -- vt7"
 alias ll="ls -l"
-alias neofetch="neofetch --ascii ./asciilogo.txt --ascii_colors '\e[0m\x1b[48;2;12;36;55;249' '\e[0m\x1b[48;2;173;28;28;249' '\e[0m\x1b[48;2;69;152;48;249' '\e[0m\x1b[48;2;35;62;139;249' '\e[0'"
+alias neofetch="neofetch --ascii /usr/share/neofetch/asciilogo.txt --ascii_colors '\e[0m\x1b[48;2;12;36;55;249' '\e[0m\x1b[48;2;173;28;28;249' '\e[0m\x1b[48;2;69;152;48;249' '\e[0m\x1b[48;2;35;62;139;249' '\e[0'"
