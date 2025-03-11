64dd7982b232b5e2ebaa91de5ce15a325bbb363d
Substring <frog2wah@gmail.com>
2025-03-11 23:00:55 +0100

    sdl2: fix 2.32.2

--- package/sdl2/sdl2.pp
+++ package/sdl2/sdl2.pp
@@ -3,6 +3,8 @@ source+="force_modes_refresh.patch"
 source+="sdl_restore_crtc_at_DestroyWindow.patch"
 prepare() {
   cd SDL2-${pkgver}
+  msg2 "Applying pipewire.patch"
+  patch -p1 < "$srcdir/pipewire.patch"
   msg2 "Applying force_modes_refresh.patch"
   patch -Np1 -i "$srcdir"/force_modes_refresh.patch
   msg2 "Applying sdl_restore_crtc_at_DestroyWindow.patch"
