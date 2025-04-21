f203bd470e46a863c76cb577a649e24b726bf6ef
Substring <frog2wah@gmail.com>
2025-04-21 12:41:33 +0200

    filesystem: avoid aliases.sh conflict

--- package/filesystem/filesystem.pp
+++ package/filesystem/filesystem.pp
@@ -1,7 +1,7 @@
-pkgrel++
-source+='aliases.sh'
+pkgrel+++
+source+='ga-aliases.sh'
 source+='groovyarcade-logo.png::https://gitlab.com/groovyarcade/branding/-/raw/main/discord/logo-discord-round.png?ref_type=heads&inline=false'
 source+='asciilogo.txt'
-files+=    ["etc/profile.d/aliases.sh"]="aliases.sh:644:0:0"
+files+=    ["etc/profile.d/ga-aliases.sh"]="ga-aliases.sh:644:0:0"
 files+=    ["usr/share/pixmaps/groovyarcade-logo.png"]="groovyarcade-logo.png:644:0:0"
 files+=    ["usr/share/neofetch/asciilogo.txt"]="asciilogo.txt:644:0:0"
