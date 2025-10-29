51546b8a58e9e91e244124508d425be5f5ff338d
Substring <frog2wah@gmail.com>
2025-10-29 19:32:18 +0100

    Revert "linux-lts: fix building doc"
    This reverts commit 582f34e93d5a12daed34c718169c49219f40faaf.

--- package/linux-lts/linux-lts.pp
+++ package/linux-lts/linux-lts.pp
@@ -7,4 +7,3 @@ source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/05_linux_15khz_amdgpu_pll_fix.patch
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/06_linux_switchres_kms_drm_modesetting.patch
 source+=https://raw.githubusercontent.com/D0023R/linux_kernel_15khz/master/linux-${_kernelversion}/07_linux_15khz_fix_ddc.patch
-source+=kernel_include.patch
