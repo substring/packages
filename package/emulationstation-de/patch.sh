04dfe7c86af5a4eb972e183ba436ea99b6e3840b
Substring <frog2wah@gmail.com>
2026-08-20 01:14:34 +0200

    fix previous fix

--- package/emulationstation-de/patch.sh
+++ package/emulationstation-de/patch.sh
@@ -1,2 +1,2 @@
-sed -i 's/\-j6//' /work/build/emulationstation-de/PKGBUILD
-sed -i 's/cmake/cmake -DDEINIT_ON_LAUNCH=on/' /work/build/emulationstation-de/PKGBUILD
+sed -i 's/\-j6//' /work/build/emulationstation-de/PKGBUILD || exit 1
+sed -i 's/cmake /cmake -DDEINIT_ON_LAUNCH=on /' /work/build/emulationstation-de/PKGBUILD || exit 1
